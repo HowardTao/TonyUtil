c5018311a88748e20e93c92b8bbd331fd7165be3
HowardTao <HowardTao@users.noreply.github.com>
2018-01-31 17:07:46 +0800

    TonyUtil.Datas.Ef

--- TonyUtil/Datas/Queries/Internal/Helper.cs
+++ TonyUtil/Datas/Queries/Internal/Helper.cs
@@ -10,8 +10,12 @@ namespace TonyUtil.Datas.Queries.Internal
     /// </summary>
    internal static class Helper
     {
-
-        public static Expression<Func<TEntity, bool>> GetWhereInfNotEmptyExpression<TEntity>(
+        /// <summary>
+        /// 获取查询条件表达式
+        /// </summary>
+        /// <param name="predicate">查询条件,如果参数值为空，则忽略该查询条件，范例：t => t.Name == ""，该查询条件被忽略。
+        /// 注意：一次仅能添加一个条件，范例：t => t.Name == "a" &amp;&amp; t.Mobile == "123"，不支持，将抛出异常</param>
+        public static Expression<Func<TEntity, bool>> GetWhereIfNotEmptyExpression<TEntity>(
             Expression<Func<TEntity, bool>> predicate) where TEntity : class
         {
             if (predicate == null) return null;
