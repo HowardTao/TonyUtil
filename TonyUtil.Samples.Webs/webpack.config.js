00c9ce19351820d50920039ca564dd8be052727a
HowardTao <527468945@qq.com>
2018-01-31 21:28:48 +0800

    测试

--- TonyUtil.Samples.Webs/webpack.config.js
+++ TonyUtil.Samples.Webs/webpack.config.js
@@ -1,84 +1,87 @@
-const path = require('path');
+const pathPlugin = require('path');
 const webpack = require('webpack');
-const merge = require('webpack-merge');
-const AotPlugin = require('@ngtools/webpack').AotPlugin;
-const CheckerPlugin = require('awesome-typescript-loader').CheckerPlugin;
+var Extract = require("extract-text-webpack-plugin");
 
 module.exports = (env) => {
-    // Configuration in common to both client-side and server-side bundles
-    const isDevBuild = !(env && env.prod);
-    const sharedConfig = {
-        stats: { modules: false },
-        context: __dirname,
-        resolve: { extensions: [ '.js', '.ts' ] },
+    //是否开发环境
+    const isDev = !(env && env.prod);
+
+    //将css提取到单独文件中
+    const extractCss = new Extract("app.css");
+
+    //获取路径
+    function getPath(path) {
+        return pathPlugin.join(__dirname, path);
+    }
+
+    //打包js
+    let jsConfig = {
+        entry: { app: getPath("Typings/main.ts") },
         output: {
-            filename: '[name].js',
-            publicPath: 'dist/' // Webpack dev middleware, if enabled, handles requests for this URL prefix
+            publicPath: 'dist/',
+            path: getPath("wwwroot/dist"),
+            filename: "[name].js"
         },
+        resolve: {
+            extensions: ['.js', '.ts']
+        },
+        devtool: "source-map",
         module: {
             rules: [
-                { test: /\.ts$/, use: isDevBuild ? ['awesome-typescript-loader?silent=true', 'angular2-template-loader', 'angular2-router-loader'] : '@ngtools/webpack' },
-                { test: /\.html$/, use: 'html-loader?minimize=false' },
-                { test: /\.css$/, use: [ 'to-string-loader', isDevBuild ? 'css-loader' : 'css-loader?minimize' ] },
-                { test: /\.(png|jpg|jpeg|gif|svg)$/, use: 'url-loader?limit=25000' }
+                { test: /\.ts$/, use: ['awesome-typescript-loader?silent=true', 'angular-router-loader'] }
             ]
         },
-        plugins: [new CheckerPlugin()]
-    };
-
-    // Configuration for client-side bundle suitable for running in browsers
-    const clientBundleOutputDir = './wwwroot/dist';
-    const clientBundleConfig = merge(sharedConfig, {
-        entry: { 'main-client': './ClientApp/boot.browser.ts' },
-        output: { path: path.join(__dirname, clientBundleOutputDir) },
         plugins: [
             new webpack.DllReferencePlugin({
-                context: __dirname,
+                manifest: require('./wwwroot/dist/polyfills-manifest.json')
+            }),
+            new webpack.DllReferencePlugin({
                 manifest: require('./wwwroot/dist/vendor-manifest.json')
-            })
-        ].concat(isDevBuild ? [
-            // Plugins that apply in development builds only
-            new webpack.SourceMapDevToolPlugin({
-                filename: '[file].map', // Remove this line if you prefer inline source maps
-                moduleFilenameTemplate: path.relative(clientBundleOutputDir, '[resourcePath]') // Point sourcemap entries to the original file locations on disk
-            })
-        ] : [
-            // Plugins that apply in production builds only
-            new webpack.optimize.UglifyJsPlugin(),
-            new AotPlugin({
-                tsConfigPath: './tsconfig.json',
-                entryModule: path.join(__dirname, 'ClientApp/app/app.browser.module#AppModule'),
-                exclude: ['./**/*.server.ts']
-            })
-        ])
-    });
-
-    // Configuration for server-side (prerendering) bundle suitable for running in Node
-    const serverBundleConfig = merge(sharedConfig, {
-        resolve: { mainFields: ['main'] },
-        entry: { 'main-server': './ClientApp/boot.server.ts' },
-        plugins: [
+            }),
             new webpack.DllReferencePlugin({
-                context: __dirname,
-                manifest: require('./ClientApp/dist/vendor-manifest.json'),
-                sourceType: 'commonjs2',
-                name: './vendor'
-            })
-        ].concat(isDevBuild ? [] : [
-            // Plugins that apply in production builds only
-            new AotPlugin({
-                tsConfigPath: './tsconfig.json',
-                entryModule: path.join(__dirname, 'ClientApp/app/app.server.module#AppModule'),
-                exclude: ['./**/*.browser.ts']
-            })
-        ]),
-        output: {
-            libraryTarget: 'commonjs',
-            path: path.join(__dirname, './ClientApp/dist')
-        },
-        target: 'node',
-        devtool: 'inline-source-map'
-    });
+                manifest: require('./wwwroot/dist/util-manifest.json')
+            }),
+            new webpack.optimize.ModuleConcatenationPlugin()
+        ].concat(isDev ? [] : [
+            new webpack.optimize.UglifyJsPlugin()
+        ])
+    }
 
-    return [clientBundleConfig, serverBundleConfig];
-};
+    //打包css
+    let cssConfig = {
+        entry: { app: getPath("wwwroot/css/main.scss") },
+        output: {
+            publicPath: './',
+            path: getPath("wwwroot/dist"),
+            filename: "[name].css"
+        },
+        resolve: {
+            modules: ['wwwroot']
+        },
+        devtool: "source-map",
+        module: {
+            rules: [
+                {
+                    test: /\.scss$/, use: extractCss.extract({
+                        use: isDev ? ['css-loader', { loader: 'postcss-loader', options: { plugins: [require('autoprefixer')] } }, 'sass-loader']
+                            : ['css-loader?minimize', { loader: 'postcss-loader', options: { plugins: [require('autoprefixer')] } }, 'sass-loader']
+                    })
+                },
+                {
+                    test: /\.(png|jpg|gif|woff|woff2|eot|ttf|svg)(\?|$)/, use: {
+                        loader: 'url-loader',
+                        options: {
+                            limit: 20000,
+                            name: "[name].[ext]",
+                            outputPath: "images/"
+                        }
+                    }
+                }
+            ]
+        },
+        plugins: [
+            extractCss
+        ]
+    }
+    return [jsConfig, cssConfig];
+}
