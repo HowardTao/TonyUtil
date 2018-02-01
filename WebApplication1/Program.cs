2e4344feb6f265a7b06c680ee74b50e9007a69f5
HowardTao <HowardTao@users.noreply.github.com>
2018-02-01 09:13:22 +0800

    删除样例

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace TonyUtil.Samples.Webs
+namespace WebApplication1
 {
     public class Program
     {
